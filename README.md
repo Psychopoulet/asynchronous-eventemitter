9b934228404ca716d10a6972af776c354755b270
DESKTOP-PVKHNCN\meona <svidal1@free.fr>
2017-01-16 13:56:35 +0100

    + de controles et ajustement de la doc

--- README.md
+++ README.md
@@ -10,20 +10,20 @@ $ npm install asynchronous-eventemitter
 ## Doc
 
 * see [node events documentation](https://nodejs.org/api/events.html)
-* ``` on("eventerror", () => { try {} catch(e) { console.log(e); } }) : return this ``` fire if an error has thrown in a callback
-
-note : use a "try/catch" in "eventerror" event to avoid loop
+* fire "error" event if an error has thrown in an event listener
+* you can now chain "emit" events, the method return the class' pointer
+* because of encapsulation, "removeListener" will not work. Please use removeAllListeners or return the right encapsuled event with "listeners" to use it.
 
 ## Examples
 
 ```js
 const events = require('asynchronous-eventemitter');
 
-new events().on("eventerror", function(err) {
+new events().on("error", function(err) {
   console.log(err);
 }).on("test", function(arg1, arg2, arg3) {
   console.log(arg1, arg2, arg3);
-}).emit("test", "arg1", "arg2", "arg3");
+}).emit("test", "arg1", "arg2", "arg3").emit("test2");
 ```
 
 ## Tests
--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,99 @@
 
 "use strict";
 
-// deps
+	// deps
 
-	const	path = require("path"),
+	const path = require("path");
+	const gulp = require("gulp");
+	const plumber = require("gulp-plumber");
+	const excludeGitignore = require("gulp-exclude-gitignore");
 
-			gulp = require("gulp"),
-			plumber = require("gulp-plumber"),
-			excludeGitignore = require("gulp-exclude-gitignore"),
+	const eslint = require("gulp-eslint");
 
-			eslint = require("gulp-eslint"),
+	const babel = require("gulp-babel");
 
-			babel = require("gulp-babel"),
-
-			mocha = require("gulp-mocha");
+	const mocha = require("gulp-mocha");
 
 	require("babel-preset-es2015-node4");
 
 // private
 
-	var _gulpFile = path.join(__dirname, "gulpfile.js"),
-		_libFiles = path.join(__dirname, "lib", "**", "*.js"),
-		_distFiles = path.join(__dirname, "dist", "**", "*.js"),
-		_unitTestsFiles = path.join(__dirname, "tests", "**", "*.js"),
-		_allJSFiles = [_gulpFile, _libFiles, _distFiles, _unitTestsFiles];
+	var _gulpFile = path.join(__dirname, "gulpfile.js");
+	var _libFiles = path.join(__dirname, "lib", "**", "*.js");
+	var _distFiles = path.join(__dirname, "dist", "**", "*.js");
+	var _unitTestsFiles = path.join(__dirname, "tests", "**", "*.js");
+	var _allJSFiles = [_gulpFile, _libFiles, _distFiles, _unitTestsFiles];
 
 // tasks
 
-	gulp.task("babel", function () {
+gulp.task("babel", function () {
 
-		return gulp.src(_libFiles)
-			.pipe(babel({
-				presets: ["es2015-node4"]
-			}))
-			.pipe(gulp.dest("dist"));
+	return gulp.src(_libFiles)
+		.pipe(babel({
+			presets: ["es2015-node4"]
+		}))
+		.pipe(gulp.dest("dist"));
 
-	});
+});
 
-	gulp.task("eslint", ["babel"], function () {
+gulp.task("eslint", ["babel"], function () {
 
-		return gulp.src(_allJSFiles)
-			.pipe(plumber())
-			.pipe(excludeGitignore())
-			.pipe(eslint({
-				"rules": {
-					"linebreak-style": 0,
-					"indent": 0
-				},
-				"env": {
-					"node": true, "es6": true, "mocha": true
-				},
-				"extends": "eslint:recommended"
-			}))
-			.pipe(eslint.format())
-			.pipe(eslint.failAfterError());
+	return gulp.src(_allJSFiles)
+		.pipe(plumber())
+		.pipe(excludeGitignore())
+		.pipe(eslint({
 
-	});
+			// http://eslint.org/docs/rules/
 
-	gulp.task("mocha", ["eslint"], function () {
+			"rules": {
 
-		return gulp.src(_unitTestsFiles)
-			.pipe(plumber())
-			.pipe(mocha({reporter: "spec"}));
+				// strict
+				"strict": ["error", "global"],
 
-	});
+				// style
+				"linebreak-style": ["error", "unix"],
+				"quotes": ["error", "double"],
+
+				// strict compare
+				"eqeqeq": ["error", "always"],
+
+				// rules
+				"default-case": "error",
+				"no-alert": "error",
+				"no-eval": "error",
+				"no-extend-native": "error",
+				"no-implied-eval": "error",
+				"no-multi-spaces": "error"
+
+			},
+
+			"env": {
+				"node": true, "es6": true, "mocha": true
+			},
+
+			"extends": "eslint:recommended"
+
+		}))
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+
+});
+
+gulp.task("mocha", ["eslint"], function () {
+
+	return gulp.src(_unitTestsFiles)
+		.pipe(plumber())
+		.pipe(mocha({reporter: "spec"}));
+
+});
 
 // watcher
 
-	gulp.task("watch", function () {
-		gulp.watch(_allJSFiles, ["mocha"]);
-	});
+gulp.task("watch", function () {
+	gulp.watch(_allJSFiles, ["mocha"]);
+});
 
 
 // default
 
-	gulp.task("default", ["mocha"]);
-	
+gulp.task("default", ["mocha"]);
